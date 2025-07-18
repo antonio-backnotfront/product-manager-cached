75c7e96bb0028787a2f654d9fa18e951a0808005
Anton Solianyk <solyanicks@gmail.com>
2025-07-19 01:55:52 +0200

    initial commit

---
+++ src/main/java/com/example/productmanagercached/ProductManagerCachedApplication.java
@@ -0,0 +1,13 @@
+package com.example.productmanagercached;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerCachedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagerCachedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productmanagercached/ProductManagerCachedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productmanagercached;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerCachedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
